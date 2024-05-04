a8a3fd1b98eac07c624f322e3c1a2681386f25af
Andres Ramirez <andres.ramirez9912@gmail.com>
2024-05-04 09:31:27 -0500

    Add List Post Code

--- x/kiichain/keeper/query_list_post.go
+++ x/kiichain/keeper/query_list_post.go
@@ -2,11 +2,13 @@ package keeper
 
 import (
 	"context"
+	"kiichain/x/kiichain/types"
 
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"kiichain/x/kiichain/types"
 )
 
 func (k Keeper) ListPost(goCtx context.Context, req *types.QueryListPostRequest) (*types.QueryListPostResponse, error) {
@@ -14,10 +16,25 @@ func (k Keeper) ListPost(goCtx context.Context, req *types.QueryListPostRequest)
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	var posts []types.Post
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	store := ctx.KVStore(k.storeKey)
+	postStore := prefix.NewStore(store, types.KeyPrefix(types.PostKey))
 
-	return &types.QueryListPostResponse{}, nil
+	pageRes, err := query.Paginate(postStore, req.Pagination, func(key []byte, value []byte) error {
+		var post types.Post
+		if err := k.cdc.Unmarshal(value, &post); err != nil {
+			return err
+		}
+
+		posts = append(posts, post)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryListPostResponse{Post: posts, Pagination: pageRes}, nil
 }
